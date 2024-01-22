316c49e827233d0e60ffc361a16ac4706e15fa92
Corey Sawin <sawincp@gmail.com>
2024-01-22 11:58:44 -0500

    create DB schema

---
+++ db/schema.rb
@@ -0,0 +1,52 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2024_01_22_165151) do
+
+  create_table "consoles", force: :cascade do |t|
+    t.string "platform"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "games", force: :cascade do |t|
+    t.string "title"
+    t.string "cover_art"
+    t.integer "release_date"
+    t.string "notes"
+    t.integer "user_id", null: false
+    t.integer "console_id", null: false
+    t.integer "genre_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["console_id"], name: "index_games_on_console_id"
+    t.index ["genre_id"], name: "index_games_on_genre_id"
+    t.index ["user_id"], name: "index_games_on_user_id"
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "type"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "games", "consoles"
+  add_foreign_key "games", "genres"
+  add_foreign_key "games", "users"
+end
