bf0ed32853c0507ca4b54666f45878d7f66cc5a6
Corey Sawin <sawincp@gmail.com>
2024-01-25 15:34:35 -0500

    import RecoilRoot

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { RecoilRoot } from 'recoil'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -11,7 +12,9 @@ const root = createRoot(domNode);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
     </BrowserRouter>
   </React.StrictMode>
 );
